f64627b9e300edbbf384d1a4db465b131caa1e9c
Álvaro Fernández <avunotfound@gmail.com>
2021-11-26 10:20:42 +0100

    Clean DniValidator

--- src/Clases/DniValidator.java
+++ src/Clases/DniValidator.java
@@ -4,18 +4,13 @@ public class DniValidator {
     private String dni;
     private final int DNI_LENGTH=9;
 
-    /**
-     * Constructor de la clase del validador de DNI, en el caso de que el string sea menor
-     * @param inputDni
-     */
     public DniValidator(String inputDni) {
-//        if(inputDni.length()<DNI_LENGTH){
-//            for(int i=0; i<(DNI_LENGTH*2-inputDni.length()); i++){
-//                inputDni+="U";
-//            }
-//        }
         this.dni = inputDni;
     }
+
+    /**
+     * valida que el DNI cumpla las características de un DNI real
+     */
     public boolean validar() {
         String letraMayuscula;
         boolean valido=false;
@@ -27,9 +22,13 @@ public class DniValidator {
         }
         return valido;
     }
+
+    /**
+     * Valida que los 8 primeros caracteres del DNI sean numeros
+     */
     private boolean soloNumeros() {
         boolean valido=true;
-        int i, j = 0;
+        int i, j;
         String numero;
         String miDNI = "";
         String[] unoNueve = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
@@ -44,12 +43,15 @@ public class DniValidator {
         }
         return valido;
     }
+
+    /**
+     * Comprueba que la letra del dni sea una valida y se corresponda con la cadena de numeros del dni
+     */
     private String letraDNI() {
         int miDNI = Integer.parseInt(this.dni.substring(0, 8));
-        int resto = 0;
-        String miLetra = "";
+        String miLetra;
         String[] asignacionLetra = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
-        resto = miDNI % 23;
+        int resto = miDNI % 23;
         miLetra = asignacionLetra[resto];
         return miLetra;
     }

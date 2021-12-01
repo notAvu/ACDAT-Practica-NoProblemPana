a0d39a94b61884e165f23f61e41ef731a1baf288
Álvaro Fernández <avunotfound@gmail.com>
2021-12-01 09:42:37 +0100

    El javadoc cuestionando a Clean Eastwood

--- src/Clases/TlfnValidator.java
+++ src/Clases/TlfnValidator.java
@@ -33,13 +33,13 @@ public class TlfnValidator {
         String auxNumber;
         StringBuilder tlfn = new StringBuilder();
         String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-        for (i = 0; i < this.telephoneNumber.length()-1; i++) {
+        for (i = 0; i < this.telephoneNumber.length(); i++) {
             auxNumber = this.telephoneNumber.substring(i, i + 1);
             for (j = 0; j < numbers.length; j++) {
                 if (auxNumber.equals(numbers[j])) tlfn.append(numbers[j]);
             }
         }
-        if (tlfn.length() != 8) justNumbers = false;
+        if (tlfn.length() != NUMBER_SIZE) justNumbers = false;
         return justNumbers;
     }
 }
